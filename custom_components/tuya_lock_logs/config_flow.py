169b117a12bfea453bbcbe86e49c0b6dce103cfb
Miguel Platas <miguel.platas@gmail.com>
2026-06-14 09:32:39 -0600

    Fix options flow for HA 2026 and bump version to 1.0.2

--- custom_components/tuya_lock_logs/config_flow.py
+++ custom_components/tuya_lock_logs/config_flow.py
@@ -84,13 +84,10 @@ class TuyaLockLogsConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     @staticmethod
     @callback
     def async_get_options_flow(config_entry):
-        return TuyaLockLogsOptionsFlow(config_entry)
+        return TuyaLockLogsOptionsFlow()
 
 
 class TuyaLockLogsOptionsFlow(config_entries.OptionsFlow):
-    def __init__(self, config_entry):
-        self.config_entry = config_entry
-
     async def async_step_init(self, user_input=None):
         if user_input is not None:
             return self.async_create_entry(title="", data=user_input)
